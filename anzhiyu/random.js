382b56fd507544ee279d2b80c70e4f873666682c
ytang4456 <ytang4456@gmail.com>
2025-06-04 14:11:22 +0800

    Site updated: 2025-06-04 14:11:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/04/phrases/","2025/06/03/hello-world/","2025/06/04/你不了解的缘由/","2025/06/04/路边的小女孩/"];function toRandomPost(){
+var posts=["2025/06/03/hello-world/","2025/06/04/phrases/","2025/06/04/你不了解的缘由/","2025/06/04/路边的小女孩/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
